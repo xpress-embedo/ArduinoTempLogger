e4c9b2105c8846873a2cdb440a78770fef0379e1
xpress_embedo <matlab.academy@gmail.com>
2017-10-13 16:15:18 +0530

    Proteus Design File Added. Basic Code Written to test LED and Serial Port is also included.

--- TempLogger/TempLogger.ino
+++ TempLogger/TempLogger.ino
@@ -1,9 +1,34 @@
 #include <SD.h>
 
+/*Led Pin*/
+const int led = 13;
+static boolean led_state = false;
+static unsigned long led_timestamp = 0;
+
 void setup() 
 {
+  Serial.begin(9600);
+  pinMode( led, OUTPUT);
+  Serial.println("Hello World");
+  led_timestamp = millis();
 }
 
 void loop() 
 {
+  /*Led Task*/
+  if ( millis()-led_timestamp >= 1000u )
+  {
+    led_timestamp = millis();
+    if ( led_state )
+    {
+      led_state = false;
+      digitalWrite( led, HIGH);
+    }
+    else
+    {
+      led_state = true;
+      digitalWrite( led, LOW);
+    }
+  }
+  /*Led Task End*/
 }
